d8c2d7aa2f3612d28efb6076bff22211c0dbd555
Disha Parale <paraledisha20@gmail.com>
2023-09-08 00:22:13 +0530

    home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 const Home = () => {
+  const [tree,setTree] = useState(false);
+  const changeOpacity = ( ) => {
+    if(window.scrollY >= 60 ){
+      setTree(true)
+    }else{
+      setTree(false)
+    }
+  } 
+  window.addEventListener('scroll',changeOpacity)
   return (
     <div className="container">
-      <div className="tree">
+      <div  className={ tree ? 'tree scrolled' : 'tree'}>
         <img src="./images/BAMBOO.png" alt="" />
       </div>
       <div className="logo">
@@ -12,6 +21,8 @@ const Home = () => {
         <h2>- By Aishwarya</h2>
       </div>
       <div className="about">
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum facilis quae, harum labore illum, nobis explicabo laudantium ea quo deleniti commodi quisquam molestiae nam mollitia asperiores? Rerum consequatur at enim error molestias delectus voluptatibus quibusdam expedita, nam officiis. Dolorem repellat cum expedita assumenda voluptatem iste, quasi sunt esse ipsam doloremque fugiat nostrum dicta voluptates vel nisi iusto velit voluptatum, qui cupiditate facere. Dolorem tenetur voluptates molestiae officia inventore? Quibusdam illo debitis libero itaque! Vitae distinctio exercitationem dolores officia suscipit, blanditiis commodi repellendus necessitatibus nam? Ullam, in repellendus aliquam delectus sequi, nemo incidunt, aliquid atque esse modi quia quasi provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis velit est reiciendis n</p>
         
       </div>
     </div>
